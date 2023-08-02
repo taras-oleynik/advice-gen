e043e94f95881397900c3b559d968861bbf333c0
Taras Oleynik <taras.oleynik@gmail.com>
2023-08-02 12:14:13 +0300

    advice gen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import icon from "./images/icon-dice.svg";
+import { BsFillPauseFill } from "react-icons/bs";
 
 function App() {
+  const [advice, setAdvice] = React.useState("");
+
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => setAdvice(data.slip))
+      .catch((error) => console.error("Error fetching advice:", error));
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="adv-frame">
+        <p className="adv-id">ADVICE #{advice.id}</p>
+        <p> "{advice.advice}"</p>
+        <div className="devider-container">
+          <hr />
+          <BsFillPauseFill />
+        </div>
+      </div>
+      <button className="randAdv" onClick={fetchAdvice}>
+        <img src={icon} alt="search Icon" />
+      </button>
     </div>
   );
 }
